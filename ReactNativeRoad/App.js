1998e056a314d948c223a4e9446414ab46c0c312
kenan <houkenan0620@126.com>
2018-10-26 13:05:23 +0800

    181026、app.js解释说明

--- ReactNativeRoad/App.js
+++ ReactNativeRoad/App.js
@@ -7,8 +7,24 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
+/**
+ 导入库包 StyleSheet、Text、View
+ 组件导入区： 所有用到的组件都需要事先进行导入，包括样式也需要进行导入
+ 从RN中导入需要使用的系统包，
+ */
+import
+{
+    Platform,
+    StyleSheet,
+    Text,
+    View
+    
+} from 'react-native';
+
+/**
+ 注册启动组件：组件只有注册后才能运行。
+ */
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -16,7 +32,14 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+
+
 type Props = {};
+
+/**
+ 核心代码区：所有逻辑代码编写的地方
+ */
+
 export default class App extends Component<Props> {
   render() {
     return (
@@ -28,6 +51,10 @@ export default class App extends Component<Props> {
     );
   }
 }
+
+/**
+ 组件样式区：render() 方法中用到的组件的样式，可以集中在这里编写
+ */
 
 const styles = StyleSheet.create({
   container: {
